c021d5583a65bda4dc154b8a2778ea53d5740e24
Umair Khan <omerjerk@gmail.com>
2013-06-17 15:05:54 +0530

    Finishing backend coding

--- src/tk/omerjerk/versionupgrader/MainActivity.java
+++ src/tk/omerjerk/versionupgrader/MainActivity.java
@@ -3,6 +3,7 @@ package tk.omerjerk.versionupgrader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -15,14 +16,15 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Toast; 
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 		new rootCheckAsyncTask().execute();
 	}
 
@@ -111,10 +113,10 @@ public class MainActivity extends Activity {
 		}
 		
 	}
-	
+	File tempBuildProp;
 	public void getValue(View v){
 		final Properties properties = new Properties();
-		File tempBuildProp = new File(Environment.getExternalStorageDirectory().getPath() + "/build.prop.bak");
+		tempBuildProp = new File(Environment.getExternalStorageDirectory().getPath() + "/build.prop.bak");
 		
 		try{
 			properties.load(new FileInputStream(tempBuildProp));
@@ -131,6 +133,62 @@ public class MainActivity extends Activity {
     	for (int i=0; i <pTitle.length; i++){
     		System.out.println("Property : " + pTitle[i] + "  Value : " + pDesc.get(i));
     	}
+	}
+	
+	public void editValue(View v){
+		
+		final Properties properties = new Properties();
+		try{
+			
+			FileInputStream in = new FileInputStream(tempBuildProp);
+			properties.load(in);
+			in.close();
+		} catch (IOException e){
+			showToast("Error : " + e);
+		}
+		
+		properties.setProperty("ro.build.version.release", "4.3");
+		
+		try {
+			FileOutputStream changedOutput = new FileOutputStream(tempBuildProp);
+			properties.store(changedOutput, null);
+			changedOutput.close();
+			System.out.println("storing properties");
+		} catch(IOException e){
+			showToast("Error : " + e);
+		}
+		
+	}
+	
+	public void copyToSystem (View v){
+		Process process = null;
+        DataOutputStream os = null;
+        
+        try {
+            process = Runtime.getRuntime().exec("su");
+	        os = new DataOutputStream(process.getOutputStream());
+	        os.writeBytes("mount -o remount,rw -t yaffs2 /dev/block/mtdblock4 /system\n");
+	        os.writeBytes("mv -f /system/build.prop /system/build.prop.bak\n");
+	        os.writeBytes("busybox cp -f " + tempBuildProp + " /system/build.prop\n");
+	        os.writeBytes("chmod 755 /system/build.prop\n");
+	        //os.writeBytes("mount -o remount,ro -t yaffs2 /dev/block/mtdblock4 /system\n");
+	        //os.writeBytes("rm " + propReplaceFile);
+	        //os.writeBytes("rm " + tempFile);
+	        os.writeBytes("exit\n");
+	        os.flush();
+	        process.waitFor();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+                process.destroy();
+            } catch (Exception e) {
+            	Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
 	}
 
 }
